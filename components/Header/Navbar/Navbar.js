f8da6ff395ef2d64341aee9222c47f5a035868e1
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-07-07 16:07:59 +0330

    add navbar elements

--- components/Header/Navbar/Navbar.js
+++ components/Header/Navbar/Navbar.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from '@/styles/navbar.module.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar">
+      <div className="logoContainer">
+        <img src="/images/logo.png" alt="logo" className="logo" />
+      </div>
+      <div className="navigationContainer">
+        <span className="vavItem">Home</span>
+        <span className="vavItem">Services</span>
+        <span className="vavItem">About me</span>
+        <span className="vavItem">Portfolio</span>
+        <span className="vavItem">Contact me</span>
+      </div>
+      <div className="hireContainer">
+        <button>Hire me</button>
+      </div>
+    </div>
+  );
 }
