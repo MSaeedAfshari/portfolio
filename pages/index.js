2c154ada450de0420f49ffe0d1e3d995ef89cc7c
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-07-15 14:53:05 +0330

    import personal card

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Header/Navbar/Navbar";
+import PersonalCard from "@/components/Header/PersonalCard/PersonalCard";
 import React from "react";
 
 export default function index() {
   return (
     <>
       <Navbar />
+      <PersonalCard />
     </>
   );
 }
