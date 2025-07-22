711ef3923f2f6c118f802688c24b71da5c8cbe20
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-07-22 15:27:38 +0330

    refactor the services

--- components/Services/Card/Card.js
+++ components/Services/Card/Card.js
@@ -1,20 +1,13 @@
 import React from "react";
 import styles from "@/styles/card.module.css";
-import { PiPark } from "react-icons/pi";
-import { CgWebsite } from "react-icons/cg";
-import { HiChip } from "react-icons/hi";
-import { LuMicrochip } from "react-icons/lu";
-import { GiMechanicalArm } from "react-icons/gi";
-import { BsBadge3dFill } from "react-icons/bs";
 
-export default function Card() {
+export default function Card({icons, title, description}) {
   return (
     <div className={styles.card}>
-      <HiChip className={styles.icon} />
-      <h3 className={styles.cardTitle}>Embedded systems</h3>
+      {icons}
+      <h3 className={styles.cardTitle}>{title}</h3>
       <p className={styles.par}>
-        Lorem ipsum dolor sit amet . Imperdiet Lorem ipsum dolor sit amet
-        consectetur
+        {description}
       </p>
     </div>
   );
--- components/Services/SeviceContainer/ServiceContainer.js
+++ components/Services/SeviceContainer/ServiceContainer.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Card from "../Card/Card";
 import styles from "@/styles/servicecontainer.module.css";
+import { PiPark } from "react-icons/pi";
+import { CgWebsite } from "react-icons/cg";
+import { HiChip } from "react-icons/hi";
+import { LuMicrochip } from "react-icons/lu";
+import { GiMechanicalArm } from "react-icons/gi";
+import { BsBadge3dFill } from "react-icons/bs";
 
 export default function ServiceContainer() {
   return (
@@ -11,12 +17,36 @@ export default function ServiceContainer() {
         world of engineering
       </p>
       <div className={styles.cardContainer}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          icons={<HiChip size={100} color="#C02C27" />}
+          title="Embedded systems"
+          description="Crafting low-level solutions that drive performance, precision, and control."
+        />
+        <Card
+          icons={<CgWebsite size={100} color="#C02C27" />}
+          title="Web development"
+          description="Creating clean, user-friendly interfaces that bring ideas to life online."
+        />
+        <Card
+          icons={<LuMicrochip size={100} color="#C02C27" />}
+          title="Electronics"
+          description="Designing and developing reliable electronic circuits for real-world applications."
+        />
+        <Card
+          icons={<GiMechanicalArm size={100} color="#C02C27" />}
+          title="Mechanical design"
+          description="Transforming ideas into precise, manufacturable mechanical designs."
+        />
+        <Card
+          icons={<BsBadge3dFill size={100} color="#C02C27" />}
+          title="3D printing"
+          description="From prototype to product — fast, accurate, and reliable 3D printing solutions."
+        />
+        <Card
+          icons={<PiPark size={100} color="#C02C27" />}
+          title="Chill...?"
+          description="Not everything has to be code and circuits — sometimes, we just chill :)"
+        />
       </div>
     </div>
   );
