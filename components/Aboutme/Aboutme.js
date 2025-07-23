a2289de08fd4b2e727c9ffc1fd42da5aff231f9d
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-07-23 16:58:56 +0330

    add about me section

---
+++ components/Aboutme/Aboutme.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "@/styles/aboutMe.module.css";
+import { MdOutlineFileDownload } from "react-icons/md";
+
+export default function Aboutme() {
+  return (
+    <div className={styles.aboutMe}>
+      <h2 className={styles.header}>About Me</h2>
+      <span className={styles.paragraph}>
+        Embedded Systems and Electronics and Also Web Designing
+      </span>
+      <div className={styles.aboutMeDetail}>
+        <img
+          src="/Images/avatar.png"
+          alt="avatar"
+          className={styles.imgContainer}
+        />
+        <div className={styles.textContainer}>
+          <p className={styles.paragraph}>
+            I'm Mohammad Saeed Afshari, an interdisciplinary engineer and
+            developer with a strong background in Embedded Systems, Web
+            Development, and Electronics. I specialize in building efficient,
+            scalable, and real world solutions from low level hardware
+            integrations to modern web applications. With hands on experience
+            across multiple industries, I combine technical depth with creative
+            problem solving to deliver high quality results. I’m always
+            exploring new technologies and challenges to grow both
+            professionally and personally.
+          </p>
+          <button className={styles.buttonHire}>
+            <MdOutlineFileDownload size={32} />
+            <span>Download CV</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
