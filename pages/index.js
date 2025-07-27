de84ff8459fb6d978848252475123ba8124fd073
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-07-27 12:41:51 +0330

    import portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Aboutme from "@/components/Aboutme/Aboutme";
 import Navbar from "@/components/Header/Navbar/Navbar";
 import PersonalCard from "@/components/Header/PersonalCard/PersonalCard";
+import Portfolio from "@/components/Portfolio/Portfolio";
 import ServiceContainer from "@/components/Services/SeviceContainer/ServiceContainer";
 import React from "react";
 
@@ -11,6 +12,7 @@ export default function index() {
       <PersonalCard />
       <ServiceContainer />
       <Aboutme />
+      <Portfolio />
     </>
   );
 }
