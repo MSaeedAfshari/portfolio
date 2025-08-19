9133b4cd53db23c55de8c59a5735dedc46481330
Mohammad Saeed Afshari <msafshari11235@gmail.com>
2025-08-19 17:40:06 +0330

    Viewport update

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,15 @@ import PersonalCard from "@/components/Header/PersonalCard/PersonalCard";
 import Portfolio from "@/components/Portfolio/Portfolio";
 import ServiceContainer from "@/components/Services/SeviceContainer/ServiceContainer";
 import React from "react";
+import Head from "next/head";
 
 export default function index() {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Mohammad Saeed Afshari</title>
+      </Head>
       <Navbar />
       <PersonalCard />
       <ServiceContainer />
