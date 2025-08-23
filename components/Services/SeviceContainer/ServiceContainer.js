110bf5c141592de8c6cc7fe9b84b6fc121876f16
Mohammad Saeed Afshari <msafshari11235@gmail.com>
2025-08-23 13:14:01 +0330

    change some paramether based on device

--- components/Services/SeviceContainer/ServiceContainer.js
+++ components/Services/SeviceContainer/ServiceContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import styles from "@/styles/servicecontainer.module.css";
 import { PiPark } from "react-icons/pi";
@@ -7,8 +7,18 @@ import { HiChip } from "react-icons/hi";
 import { LuMicrochip } from "react-icons/lu";
 import { GiMechanicalArm } from "react-icons/gi";
 import { BsBadge3dFill } from "react-icons/bs";
+import { isMobile } from "@/Utils/isMobile";
 
 export default function ServiceContainer() {
+  const [isDeviceMobile, setIsDeviceMobile] = useState(isMobile());
+  const [iconSize, setIconSize] = useState();
+
+  useEffect(() => {
+    setIsDeviceMobile(isMobile());
+    isDeviceMobile ? setIconSize(50) : setIconSize(100);
+  });
+
+
   return (
     <div className={styles.serviceContainer}>
       <h2 className={styles.header}>Services</h2>
@@ -18,32 +28,32 @@ export default function ServiceContainer() {
       </p>
       <div className={styles.cardContainer}>
         <Card
-          icons={<HiChip size={100} color="#C02C27" />}
+          icons={<HiChip size={iconSize} color="#C02C27" />}
           title="Embedded systems"
           description="Crafting low-level solutions that drive performance, precision, and control."
         />
         <Card
-          icons={<CgWebsite size={100} color="#C02C27" />}
+          icons={<CgWebsite size={iconSize} color="#C02C27" />}
           title="Web development"
           description="Creating clean, user-friendly interfaces that bring ideas to life online."
         />
         <Card
-          icons={<LuMicrochip size={100} color="#C02C27" />}
+          icons={<LuMicrochip size={iconSize} color="#C02C27" />}
           title="Electronics"
           description="Designing and developing reliable electronic circuits for real-world applications."
         />
         <Card
-          icons={<GiMechanicalArm size={100} color="#C02C27" />}
+          icons={<GiMechanicalArm size={iconSize} color="#C02C27" />}
           title="Mechanical design"
           description="Transforming ideas into precise, manufacturable mechanical designs."
         />
         <Card
-          icons={<BsBadge3dFill size={100} color="#C02C27" />}
+          icons={<BsBadge3dFill size={iconSize} color="#C02C27" />}
           title="3D printing"
           description="From prototype to product — fast, accurate, and reliable 3D printing solutions."
         />
         <Card
-          icons={<PiPark size={100} color="#C02C27" />}
+          icons={<PiPark size={iconSize} color="#C02C27" />}
           title="Chill...?"
           description="Not everything has to be code and circuits. sometimes, we just chill :)"
         />
